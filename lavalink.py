4b9ac5a3a551dd963f179fbaea5f90c34e57c63d
あきしょう <akishoudayo@gmail.com>
2022-01-08 03:51:52 +0900

    Update lavalink.py

--- lavalink.py
+++ lavalink.py
@@ -2,7 +2,7 @@ from os import getenv
 from re import sub
 
 try:
-    temp = open('application.yml', 'r').read()
+    temp = open('applicate.yml', 'r').read()
     temp = sub('REPLACEPASSWORD', getenv('PASSWORD'), temp)
     open('application.yml', 'w').write(temp)
 except:
